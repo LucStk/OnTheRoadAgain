c327fabbb80c1a8cb39fe6179de4fe93428632aa
LucStk <luc.sterkers@laposte.net>
2025-06-14 12:27:17 +0200

    création d'un nouveau module pour l'interface user

---
+++ remotes/ui-auth/src/main.ts
@@ -0,0 +1,27 @@
+//import { createPinia } from 'pinia';
+//import piniaPluginPersistedstate from 'pinia-plugin-persistedstate';
+
+import { createApp } from 'vue';
+import App from './App.vue';
+
+//import router from './services/router';
+//import {api} from "./services/api";
+//import './services/intercepteur'
+
+import "flyonui/flyonui";
+import './main.css';
+import './assets/css/elements.css';
+
+async function bootstrap() {
+  //const pinia = createPinia();
+  //pinia.use(piniaPluginPersistedstate);
+  const app = createApp(App);
+  //app.use(pinia);
+
+  //app.use(VueMaplibreGl);
+  //app.use(router);
+
+  app.mount('#app');
+}
+
+bootstrap();
